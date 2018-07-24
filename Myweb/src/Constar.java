26f7c336ee344be9e3d531941450f0da36086e69
Admin <sarawut260741@gmail.com>
2018-07-24 11:39:01 +0700

    version 1

---
+++ Myweb/src/Constar.java
@@ -0,0 +1,16 @@
+
+public class Constar {
+	
+	private int num ;
+	
+public Constar (int num) {
+		this.num = num ;
+	}
+public int getNum() {
+	return num;
+}
+public void setNum(int num) {
+	this.num = num;
+}
+
+}
---
+++ Myweb/src/TestCon.java
@@ -0,0 +1,11 @@
+
+public class TestCon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Constar c = new Constar(5);
+		System.out.println("... "+c.getNum());
+	}
+
+}
